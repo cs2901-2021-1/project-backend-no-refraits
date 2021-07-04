054df9a74515aba52ac09ee91ea23de99887e0fa
miunmnn <esteban.principe@utec.edu.pe>
2021-07-03 22:53:25 -0500

    springboot first commit

---
+++ softwarebackend/src/main/java/com/example/softwarebackend/SoftwarebackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.softwarebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwarebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwarebackendApplication.class, args);
+	}
+
+}
---
+++ softwarebackend/src/test/java/com/example/softwarebackend/SoftwarebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.softwarebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwarebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
