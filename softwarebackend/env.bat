958c51cc27b0ea41333e3092016d012772ea06dd
ZzAZz4 <esteban.villacorta@utec.edu.pe>
2021-07-18 16:36:20 -0500

    removed vestigial project

--- softwarebackend/env.bat
+++
@@ -1,4 +0,0 @@
-set "SONAR_TOKEN=20daeca374ef574bf415427bcac890d63a15b31c"
-set "UTEC_DB_URL=jdbc:oracle:thin:@coreqa4.cl2cyff2cklb.us-west-2.rds.amazonaws.com:1521:coreqa4"
-set "UTEC_DB_USERNAME=desarrollo"
-set "UTEC_DB_PASSWORD=qad3vLE7Uk0110xor"
