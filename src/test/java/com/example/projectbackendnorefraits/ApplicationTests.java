ebf8e40ed1e808d26f895ee19aa787287a5a189d
ZzAZz4 <esteban.villacorta@utec.edu.pe>
2021-07-18 16:47:22 -0500

    removed unused imports

--- src/test/java/com/example/projectbackendnorefraits/ApplicationTests.java
+++ src/test/java/com/example/projectbackendnorefraits/ApplicationTests.java
@@ -1,29 +1,19 @@
 package com.example.projectbackendnorefraits;
 
-import oracle.ucp.util.Pair;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import data.entities.*;
-import data.repositories.*;
-
-import controller.*;
 import business.*;
 
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
