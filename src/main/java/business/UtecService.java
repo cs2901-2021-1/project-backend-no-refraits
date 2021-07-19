dcc6105fdeb55636cb8c94160c567b94c8719c5b
ZzAZz4 <esteban.villacorta@utec.edu.pe>
2021-07-19 08:48:28 -0500

    fix query

--- src/main/java/business/UtecService.java
+++ src/main/java/business/UtecService.java
@@ -70,56 +70,52 @@ public class UtecService {
             "WHERE PERIODO_RANGO.ISDELETED = 'N'\n" +
             "ORDER BY PERIODO_RANGO.FECHAINICIO";
 
-    public static final String QUERYCURSOS = "SELECT\n" +
+    public static final String QUERYCURSOS = "" +
+            "SELECT DISTINCT\n" +
             "            CURSO_ACT.DESCRIPCIONLARGA                                  AS \"nombre_curso\",\n" +
             "            CURSO_ACT.CODACTIVIDAD                                      AS \"cod_curso\"\n" +
             "\n" +
             "FROM        PROGRAMACION.PRO_CURSO_SECCION CURSO_SECCION\n" +
             "\n" +
-            "                INNER JOIN  GENERAL.GEN_MAESTRO_TABLAS_DETALLE CURSO_SECCION_DATOS\n" +
-            "                            ON          CURSO_SECCION.CODSECCIONMTD = CURSO_SECCION_DATOS.CODMAESTROTABLASDETALLE\n" +
-            "                                AND         CURSO_SECCION_DATOS.ISDELETED = 'N'\n" +
+            "INNER JOIN  GENERAL.GEN_MAESTRO_TABLAS_DETALLE CURSO_SECCION_DATOS\n" +
+            "ON          CURSO_SECCION.CODSECCIONMTD = CURSO_SECCION_DATOS.CODMAESTROTABLASDETALLE\n" +
+            "AND         CURSO_SECCION_DATOS.ISDELETED = 'N'\n" +
             "\n" +
-            "                INNER JOIN  (\n" +
-            "    SELECT  PCS.*\n" +
-            "         ,   NVL(UTEC.FN_GET_CODDOCENTEREASIGNADO(PCS.CODCURSOSESION), PCS.CODDOCENTE) CODRDOCENTE\n" +
-            "    FROM    PROGRAMACION.PRO_CURSO_SESION PCS\n" +
-            ")   CURSO_SESION\n" +
-            "                            ON          CURSO_SECCION.CODCURSOSECCION = CURSO_SESION.CODCURSOSECCION\n" +
-            "                                AND         CURSO_SESION.ISDELETED = 'N'\n" +
+            "INNER JOIN  PROGRAMACION.PRO_CURSO_SESION CURSO_SESION\n" +
+            "ON          CURSO_SECCION.CODCURSOSECCION = CURSO_SESION.CODCURSOSECCION\n" +
+            "AND         CURSO_SESION.ISDELETED = 'N'\n" +
             "\n" +
-            "                INNER JOIN  GENERAL.GEN_MAESTRO_TABLAS_DETALLE CURSO_SESION_DATOS\n" +
-            "                            ON          CURSO_SESION.CODTIPOSESIONMTD = CURSO_SESION_DATOS.CODMAESTROTABLASDETALLE\n" +
-            "                                AND         CURSO_SESION_DATOS.ISDELETED = 'N'\n" +
+            "INNER JOIN  GENERAL.GEN_MAESTRO_TABLAS_DETALLE CURSO_SESION_DATOS\n" +
+            "ON          CURSO_SESION.CODTIPOSESIONMTD = CURSO_SESION_DATOS.CODMAESTROTABLASDETALLE\n" +
+            "AND         CURSO_SESION_DATOS.ISDELETED = 'N'\n" +
             "\n" +
-            "                INNER JOIN  PROGRAMACION.PRO_CURSO_PERIODO CURSO_PERIODO\n" +
-            "                            ON          CURSO_SECCION.CODCURSOPERIODO = CURSO_PERIODO.CODCURSOPERIODO\n" +
-            "                                AND         CURSO_PERIODO.ISDELETED = 'N'\n" +
+            "INNER JOIN  PROGRAMACION.PRO_CURSO_PERIODO CURSO_PERIODO\n" +
+            "ON          CURSO_SECCION.CODCURSOPERIODO = CURSO_PERIODO.CODCURSOPERIODO\n" +
+            "AND         CURSO_PERIODO.ISDELETED = 'N'\n" +
             "\n" +
-            "                INNER JOIN  CONFIGURACION.CON_CURSO CURSO\n" +
-            "                            ON          CURSO_PERIODO.CODCURSO = CURSO.CODCURSO\n" +
-            "                                AND         CURSO.ISDELETED = 'N'\n" +
+            "INNER JOIN  CONFIGURACION.CON_CURSO CURSO\n" +
+            "ON          CURSO_PERIODO.CODCURSO = CURSO.CODCURSO\n" +
+            "AND         CURSO.ISDELETED = 'N'\n" +
             "\n" +
-            "                INNER JOIN  CONFIGURACION.CON_ACTIVIDAD CURSO_ACT\n" +
-            "                            ON          CURSO.CODCURSO = CURSO_ACT.CODACTIVIDAD\n" +
-            "                                AND         CURSO_ACT.ISDELETED = 'N'\n" +
+            "INNER JOIN  CONFIGURACION.CON_ACTIVIDAD CURSO_ACT\n" +
+            "ON          CURSO.CODCURSO = CURSO_ACT.CODACTIVIDAD\n" +
+            "AND         CURSO_ACT.ISDELETED = 'N'\n" +
             "\n" +
-            "                INNER JOIN  GENERAL.GEN_AREA_FUNCIONAL AREA_FUNCIONAL\n" +
-            "                            ON          CURSO_ACT.CODAREAFUNCIONAL = AREA_FUNCIONAL.CODAREAFUNCIONAL\n" +
-            "                                AND         AREA_FUNCIONAL.ISDELETED = 'N'\n" +
+            "INNER JOIN  GENERAL.GEN_AREA_FUNCIONAL AREA_FUNCIONAL\n" +
+            "ON          CURSO_ACT.CODAREAFUNCIONAL = AREA_FUNCIONAL.CODAREAFUNCIONAL\n" +
+            "AND         AREA_FUNCIONAL.ISDELETED = 'N'\n" +
             "\n" +
-            "                INNER JOIN  PROGRAMACION.PRO_PERIODORANGO RANGO_PERIODO\n" +
-            "                            ON          CURSO_PERIODO.CODPERIODORANGO = RANGO_PERIODO.CODPERIODORANGO\n" +
-            "                                AND         RANGO_PERIODO.ISDELETED = 'N'\n" +
+            "INNER JOIN  PROGRAMACION.PRO_PERIODORANGO RANGO_PERIODO\n" +
+            "ON          CURSO_PERIODO.CODPERIODORANGO = RANGO_PERIODO.CODPERIODORANGO\n" +
+            "AND         RANGO_PERIODO.ISDELETED = 'N'\n" +
             "\n" +
-            "                INNER JOIN  PROGRAMACION.PRO_PERIODO PERIODO\n" +
-            "                            ON          RANGO_PERIODO.CODPERIODO = PERIODO.CODPERIODO\n" +
-            "                                AND         PERIODO.ISDELETED = 'N'\n" +
+            "INNER JOIN  PROGRAMACION.PRO_PERIODO PERIODO\n" +
+            "ON          RANGO_PERIODO.CODPERIODO = PERIODO.CODPERIODO\n" +
+            "AND         PERIODO.ISDELETED = 'N'\n" +
             "\n" +
             "WHERE       CURSO_SECCION.ISDELETED = 'N'\n" +
-            "  AND         RANGO_PERIODO.FECHAINICIO >= TO_TIMESTAMP('2018-12-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS') AND AREA_FUNCIONAL.CODAREAFUNCIONAL=?\n" +
-            "ORDER BY    RANGO_PERIODO.FECHAINICIO\n" +
-            ";\n";
+            "AND         RANGO_PERIODO.FECHAINICIO >= TO_TIMESTAMP('2018-12-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS')\n" +
+            "AND         AREA_FUNCIONAL.CODAREAFUNCIONAL= ? \n";
 
     public final String user;
     public final String password;
@@ -166,6 +162,7 @@ public class UtecService {
 
     public List<Course> getCourseFromDireccion(String direccion) throws SQLException{
         try(var connection = this.getConnection()){
+            System.out.println(QUERYCURSOS);
             try ( PreparedStatement sentencia = connection.prepareStatement(QUERYCURSOS)){
                 System.out.println(direccion);
                 sentencia.setString(1, direccion);
