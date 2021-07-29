2c26d4e3cbde0e3aad22e7dfd0332bf59c744619
miunmnn <esteban.principe@utec.edu.pe>
2021-07-28 20:53:24 -0500

    Update aiservice.java

--- src/main/java/business/AIService.java
+++ src/main/java/business/AIService.java
@@ -21,14 +21,14 @@ public class AIService {
     String urlAIServer = "https://b8acm33416.execute-api.us-east-2.amazonaws.com/test/analytics/predict_next_semester";
 
     public PredictionResponse getPrediction (String course) throws URISyntaxException, IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
+        var client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .GET()
                 .uri(URI.create(urlAIServer + "?course=" + course))
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         Logger.getLogger(AIService.class.getName()).warning(response.body());
-        ObjectMapper mapper = new ObjectMapper();
+        var mapper = new ObjectMapper();
         return mapper.readValue(response.body(), new TypeReference<>() {});
     }
 
--- src/main/java/config/JwtAuthenticationFilter.java
+++ src/main/java/config/JwtAuthenticationFilter.java
@@ -29,6 +29,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         String username = null;
         String authToken = null;
         if (header == null) {
+            chain.doFilter(req, res);
             return;
         }
         authToken = header;
@@ -36,6 +37,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             username = jwtTokenUtil.getUsernameFromToken(authToken);
         } catch(Exception e) {
             logger.error("Error en la autenticacion");
+            chain.doFilter(req, res);
             return;
         }
 
